8e0e58621c61650a8e72761ce9814ad8b94a006c
Carlos Martinez <curlosmart@gmail.com>
2021-05-27 20:02:19 -0400

    weather can be tracked in a file now

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 require('dotenv').config()
 const got = require('got')
 const chalk = require('chalk')
+const fs = require('fs')
 
 const myArgs = process.argv.slice(2)
-const LOCATION = myArgs[0].toUpperCase()
-const TEMP_TYPE = myArgs[1].toLowerCase()
 
+if (myArgs.length < 1) {
+    console.log("Please type a valid location after node app.js. For example, 'node app.js bucharest' or 'node app.js bucharest -f'.")
+    return
+}
+
+const LOCATION = myArgs[0].toUpperCase()
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 const CURRENT_WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?q=${LOCATION}&appid=${WEATHER_API_KEY}`
 const GEOCODING_API_KEY = process.env.GEOCODING_API_KEY
 const GEOCODING_API_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${LOCATION}.json?access_token=${GEOCODING_API_KEY}`
 
-
-console.log(LOCATION, TEMP_TYPE)
 
 const getCoordinates = async () => {
     let coords = ''
@@ -51,23 +54,70 @@ const logWeatherInfo = async (url) => {
         const dailyWeather = weatherData.daily
         const expectedConditions = getMostFrequentCondition(dailyWeather)
         const placeInfo = await getCityAndCountryInfo()
+        let weatherFileInfo = {location: `${placeInfo.city}, ${placeInfo.country}`}
 
-        console.log(expectedConditions)
-        
-        if(TEMP_TYPE == '-f' || TEMP_TYPE == 'farhenheit') {
-            temp = convertToFarenheit(currentTemp)
-            tem
-            console.log(`Current temperature in ${placeInfo.city}, ${placeInfo.country} is ${chalk.yellow(temp + 'F')}.`)
-        } else if(TEMP_TYPE == '-c' || TEMP_TYPE == 'celsius') {
-            temp = convertToCelsius(currentTemp)
-            console.log(`Current temperature in ${placeInfo.city}, ${placeInfo.country} is ${chalk.blue(temp + 'C')}.`)
+
+        if (myArgs.length < 2) {
+            let tempFarenheit = convertToFarenheit(currentTemp)
+            let tempCelsius = convertToCelsius(currentTemp)
+            console.log(`Current temperature in ${placeInfo.city}, ${placeInfo.country} is ${chalk.yellow(tempFarenheit + 'F')} or ${chalk.blue(tempCelsius + 'C')}.`)
+            weatherFileInfo['farenheit'] = tempFarenheit
+            weatherFileInfo['celsius'] = tempCelsius
+
+        } else if (myArgs.length >= 2) {
+            const TEMP_TYPE = myArgs[1].toLowerCase()
+
+            console.log(TEMP_TYPE)
+            console.log(myArgs)
+
+            if(TEMP_TYPE == '-f' || TEMP_TYPE == 'farhenheit') {
+                tempFarenheit = convertToFarenheit(currentTemp)
+                console.log(`Current temperature in ${placeInfo.city}, ${placeInfo.country} is ${chalk.yellow(tempFarenheit + 'F')}.`)
+                weatherFileInfo['farenheit'] = tempFarenheit
+
+            } else if(TEMP_TYPE == '-c' || TEMP_TYPE == 'celsius') {
+                tempCelsius = convertToCelsius(currentTemp)
+                console.log(`Current temperature in ${placeInfo.city}, ${placeInfo.country} is ${chalk.blue(tempCelsius + 'C')}.`)
+                weatherFileInfo['celsius'] = tempCelsius
+            }
         }
 
-        console.log(`Conditions are currently: ${currentConditions}`)
-        console.log(`What you should expect: ${expectedConditions}`)
+        console.log(`Conditions are currently: ${currentConditions}.`)
+        console.log(`What you should expect: ${expectedConditions}.`)
+
+        weatherFileInfo['currentConditions'] = currentConditions
+        weatherFileInfo['expectedConditions'] = expectedConditions
+        addToWeatherTrackingFile(weatherFileInfo)
     } catch (error) {
         console.log(error.response.body)
     }
+}
+
+const addToWeatherTrackingFile = (weatherFileInfo) => {
+    let line = `Location: ${weatherFileInfo.location} | Temperature: `
+    let datetime = new Date()
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let currentDate = datetime.toLocaleDateString(undefined, options)
+
+    if(weatherFileInfo.hasOwnProperty('farenheit')) {
+        line += `${weatherFileInfo.farenheit}F, `
+    }
+
+    if(weatherFileInfo.hasOwnProperty('celsius')) {
+        line += `${weatherFileInfo.celsius}C | `
+    }
+
+    line += `Current Conditions: ${weatherFileInfo.currentConditions} | Expected Conditions: ${weatherFileInfo.expectedConditions} `
+    line += `| ${currentDate}\n`
+
+    console.log(line)
+    fs.appendFile('weather.txt', line, (error) => {
+        if (error) {
+            throw error;
+        }
+
+        console.log('Weather was added to your weather tracking file, weather.txt')
+    })
 }
 
 const getMostFrequentCondition = (dailyWeather) => {
